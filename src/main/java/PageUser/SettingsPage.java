1eff6858fbf274b0aaac8ffb5100030b89863b3a
Пётр <petr.mazin@avalab.io>
2018-11-07 15:15:29 +0500

    костылик для хрома, пока что так...

--- src/main/java/PageUser/SettingsPage.java
+++ src/main/java/PageUser/SettingsPage.java
@@ -48,9 +48,15 @@ public class SettingsPage extends CommonPage {
          * +"select.value = \"RU\";select.dispatchEvent(new Event('input', {
          * bubbles: true }));");
          */
+        Integer count = 0;
         while ($(birthdayField).getValue().compareToIgnoreCase("10/10/2010") != 0) {
+            delayAndSendKeysForCssElement(birthdayField, "");
+            waitSomeSeconds(1);
             delayAndSendKeysForCssElement(birthdayField, "10/10/2010");
             waitSomeSeconds(1);
+            if(count > 50)
+              break;
+            count++;
         }
         delayAndClickForCssElement(maleGendeRBtn);
         delayAndSetValueForCSSElement(phoneField, "1234456688");
