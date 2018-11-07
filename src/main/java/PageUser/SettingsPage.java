b57ecba127d2949153c8c967ec7e4b2301fee01b
Пётр <petr.mazin@avalab.io>
2018-11-07 09:32:39 +0500

    попытка получать файл через java.io.File

--- src/main/java/PageUser/SettingsPage.java
+++ src/main/java/PageUser/SettingsPage.java
@@ -4,8 +4,8 @@ import main.java.CommonPage;
 
 import org.openqa.selenium.Keys;
 
-
 import static com.codeborne.selenide.Selenide.*;
+import java.io.File;
 
 public class SettingsPage extends CommonPage {
 
@@ -31,7 +31,6 @@ public class SettingsPage extends CommonPage {
 
     private final String submitBtn = "button[type='submit']";
 
-
     public void getKYCForm() {
         $("#main-navigation");
         delayAndClickForCssElement(settingsPage);
@@ -44,8 +43,10 @@ public class SettingsPage extends CommonPage {
         delayAndSetValueForCSSElement(lastNameField, "John");
         $(countrySelect).sendKeys(Keys.ARROW_DOWN);
         /**
-         * Способ установки значения дропдауна через js
-         js.executeScript("var select = document.querySelector('select[name=\"country\"]');" +"select.value = \"RU\";select.dispatchEvent(new Event('input', { bubbles: true }));");
+         * Способ установки значения дропдауна через js js.executeScript("var
+         * select = document.querySelector('select[name=\"country\"]');"
+         * +"select.value = \"RU\";select.dispatchEvent(new Event('input', {
+         * bubbles: true }));");
          */
         while ($(birthdayField).getValue().compareToIgnoreCase("10-10-2010") != 0) {
             delayAndSendKeysForCssElement(birthdayField, "10-10-2010");
@@ -53,7 +54,8 @@ public class SettingsPage extends CommonPage {
         }
         delayAndClickForCssElement(maleGendeRBtn);
         delayAndSetValueForCSSElement(phoneField, "1234456688");
-        delayAndSendKeysForCssElement(fileInput, System.getProperty("user.dir") + "/src/test/resources/test.jpg");
+        File file = new File(System.getProperty("user.dir") + "/src/test/resources/test.jpg");
+        delayAndSendKeysForCssElement(fileInput, file.getAbsolutePath());
         existForXpathElement("//div[text()=\"test.jpg\"]");
         delayAndClickWithoutScrollForCssElement(agreeChB);
         delayAndClickWithoutScrollForCssElement(submitBtn);
